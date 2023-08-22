f598e13599841aaba0beafaa6d1779dc7247e207
jonasreyes <jonasrey@gmail.com>
2023-08-21 21:10:56 -0400

    Concluida primera etapa del Proyecto, se añaden widgets básicos, widgets de diagramas, etc..

---
+++ app/Filament/Widgets/PatientTypeOverview.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Filament\Widgets;
+
+use App\Models\Patient;
+use Filament\Widgets\StatsOverviewWidget as BaseWidget;
+use Filament\Widgets\StatsOverviewWidget\Stat;
+
+class PatientTypeOverview extends BaseWidget
+{
+    protected function getStats(): array
+    {
+        return [
+            Stat::make('Gatos', Patient::query()->where('type', 'gato')->count()),
+            Stat::make('Perros', Patient::query()->where('type', 'perro')->count()),
+            Stat::make('Conejos', Patient::query()->where('type', 'conejo')->count()),
+        ];
+    }
+}
